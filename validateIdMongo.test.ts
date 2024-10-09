a609c75e0a5644cede84e66e5ee1ccd19d6657c6
Andres Condo <1dkevinandres.b@gmail.com>
2024-10-09 11:51:38 -0500

    Adición de pruebas unitarias

---
+++ validateIdMongo.test.ts
@@ -0,0 +1,20 @@
+import { validateIdMongo } from './src/index';
+
+describe('validateIdMongo', () => {
+  it('should throw an error if id is not provided', async () => {
+    await expect(validateIdMongo('')).rejects.toThrow('you must add an id');
+  });
+
+  it('should throw an error if id is invalid', async () => {
+    await expect(validateIdMongo('invalid-id')).rejects.toThrow('the ID is wrong');
+  });
+
+  it('should not throw an error if id is valid', async () => {
+    const validId = '507f1f77bcf86cd799439011'; // Un ID de MongoDB válido
+    await expect(validateIdMongo(validId)).resolves.toBeUndefined();
+  });
+
+  it('should throw a custom error message if provided', async () => {
+    await expect(validateIdMongo('invalid-id', 'custom error message')).rejects.toThrow('custom error message');
+  });
+});
